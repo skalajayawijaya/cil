2dedd3bef54efdf0f4349dfcbe4c99b70ad12948
skalajayawijaya <skalajayawijaya@gmail.com>
2026-08-04 13:49:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -220,7 +220,7 @@ if (sendReply) {
     }
 
     try {
-      await fetch("https://script.google.com/macros/s/AKfycbz9P_6fKSFAGEQWPHW4mont7_Q81LSwm7RQdLSq2uWZTYulK4Ct9Oo_yErC-7v5V0WuKQ/exec"; {
+      await fetch("https://script.google.com/macros/s/AKfycbz9P_6fKSFAGEQWPHW4mont7_Q81LSwm7RQdLSq2uWZTYulK4Ct9Oo_yErC-7v5V0WuKQ/exec", {
         method: "POST",
         mode: "no-cors",
         headers: {
